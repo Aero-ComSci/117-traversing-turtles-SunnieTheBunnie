3f3b408fdff3f101ff142fd7703ddd606205be0b
SunnieTheBunnie <148286895+SunnieTheBunnie@users.noreply.github.com>
2024-10-15 13:47:58 -0700

    Update traversingturtles.py

--- traversingturtles.py
+++ traversingturtles.py
@@ -21,6 +21,7 @@ direction = 90
 # starting point for all the drwaings
 startx = 0
 starty = 0
+z = 5
 
 # draws each picture
 for t in my_turtles:
@@ -28,8 +29,42 @@ for t in my_turtles:
   t.penup()
   t.goto(startx, starty)
   t.pendown()
-  t.right(45)     
-  t.forward(50)
+  t.right(100-z)     
+  t.forward(50+z)
+  z=z+5
+  direction = t.heading()
+
+#	sets coordinates for the next drawing
+  startx = t.xcor()
+  starty = t.ycor()
+
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+
+for s in turtle_shapes:
+  t = trtl.Turtle(shape=s)
+  new_color = turtle_colors.pop()
+  t.fillcolor(new_color)
+  t.pencolor(new_color)
+  my_turtles.append(t)
+
+direction = 90
+
+# starting point for all the drwaings
+startx = 0
+starty = 0
+z = 5
+
+# draws each picture
+for t in my_turtles:
+  t.setheading(direction)
+  t.penup()
+  t.goto(startx, starty)
+  t.pendown()
+  t.right(100-z)     
+  t.forward(50+z)
+  z=z+5
   direction = t.heading()
 
 #	sets coordinates for the next drawing
